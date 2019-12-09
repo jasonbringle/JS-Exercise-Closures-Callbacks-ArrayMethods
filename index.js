407bf11d837b9df25f9b3252e7b431c5889b4eed
Jason <Jason@MacBook-Pro-3.local>
2019-12-09 00:11:56 -0800

    done up to processSum

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, cb) {
+  return cb(list.length);
 }
 
 /**
@@ -66,9 +66,11 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function processLastItem(stringList, cb){
+    return cb(stringList[stringList.length -1]);
+} 
+
+
 
 /**
  * ### Challenge `processSum`
@@ -87,10 +89,10 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
-}
-
+function processSum(numberList, cb) {
+  
+  return cb(numberList.reduce((a,b) => a + b,0))
+  }
 /**
  * ### Challenge `processProduct`
  * 
