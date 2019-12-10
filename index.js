543a3e6481106219cb309b558aa29e3995206e34
Jason <Jason@MacBook-Pro-3.local>
2019-12-09 23:52:20 -0800

    MVP

--- index.js
+++ index.js
@@ -180,10 +180,15 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
+
 function getFullNames(runners) {
-   return runners.forEach(function(info){
-      return (`${info.first_name}, ${info.last_name}`)})
-}; 
+  const allRunners = [];
+  runners.forEach(function(runner) {
+      const firLaName = `${runner.last_name}, ${runner.first_name}`;
+      allRunners.push(firLaName);
+    });
+    return allRunners;
+} 
 
 /**
  * ### Challenge `firstNamesAllCaps`
@@ -229,7 +234,9 @@ function getRunnersByTShirtSize(runners,tShirtSize) {
  * @returns a number which is the sum of the donations by all runners.
 */
 function tallyUpDonations(runners) {
- return runners.reduce((cashola, givers) => givers.donations + cashola,0)
+ return runners.reduce((cashola, givers) => {
+   return givers.donation + cashola;
+  },0)
 }
 
 /////////////// CLOSURES ///////////////
@@ -250,9 +257,10 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = 0;
+  return function() {
+    // count = count +1;
+    return count++;
   }
   // BROKEN CODE ENDS
 }
@@ -277,10 +285,17 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(max) {
+let count = 0;
+return function(){
+  if (count > max) {
+    count = 0;
+    }
+  return count++;
+  }
 }
-
+const counter = counterMakerWithLimit()
+console.log(counter);
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
